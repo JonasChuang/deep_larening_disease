218c264af54d0b6db1bba20dbbe1165185fd990e
Jonas <tc1752202@tzuchi.com.tw>
2025-09-19 16:53:36 +0800

    ..

--- Hyperkalemia/Hyperkalemia.py
+++ Hyperkalemia/Hyperkalemia.py
@@ -60,16 +60,21 @@ tf.random.set_seed(RANDOM_STATE)
 
 # ======== 特徵定義 ========
 VITAL_LABELS = [
-    'HEART RATE',#220045
-    'RESPIRATORY RATE', #220210
+    'HEART RATE',#220045:心跳
+    'RESPIRATORY RATE', #220210:呼吸
     'SPO2',
-    'TEMPERATURE CELSIUS', #223762
-    'NON INVASIVE BLOOD PRESSURE SYSTOLIC', # 220179
-    'NON INVASIVE BLOOD PRESSURE DIASTOLIC', #220180
-    'MEAN ARTERIAL PRESSURE (NIBP)'
+    'TEMPERATURE CELSIUS', #223762:體溫
+    'NON INVASIVE BLOOD PRESSURE SYSTOLIC', # 220179:非侵入式收縮壓
+    'NON INVASIVE BLOOD PRESSURE DIASTOLIC', #220180:非侵入式舒張壓
+    'MEAN ARTERIAL PRESSURE (NIBP)'#非侵入式平均動脈壓 (MAP)
 ]
 LAB_LABELS = [  # 排除 POTASSIUM，避免洩漏
-    'SODIUM', 'BICARBONATE', 'CHLORIDE', 'CREATININE', 'UREA NITROGEN', 'GLUCOSE'
+    'SODIUM'#鈉
+    , 'BICARBONATE'#CO2 檢驗
+    , 'CHLORIDE'#氯離子檢驗
+    , 'CREATININE'
+    , 'UREA NITROGEN'#血液尿素氮，BUN
+    , 'GLUCOSE'
 ]
 FEATURE_MAP = {
     'HEART RATE':'hr','RESPIRATORY RATE':'rr','SPO2':'spo2','TEMPERATURE CELSIUS':'temp',
@@ -674,29 +679,3 @@ def main():
 
 
 
-# 訓練深度學習模型（例如用 MIMIC ICU 病人做 CKD / 低鈉 / 敗血症預測）分成三個集合：
-# 1️⃣ 訓練集 (Training set)
-# 用來 訓練模型參數（例如 GRU 裡的權重）。
-# 模型會一邊看訓練集、一邊更新權重，讓 loss 下降。
-
-# 2️⃣ 驗證集 (Validation set)
-# 不參與訓練，只在 每個 epoch 結束後用來檢查模型表現。
-# 功能：
-# 幫助你調整 超參數（learning rate、batch size、dropout…）。
-# 早停 (early stopping)：如果驗證集 loss 開始上升，代表模型 overfitting，要停下來。
-# 選擇最佳模型 checkpoint。
-# ✅ 重點：驗證集模擬「模型在沒看過的新資料」的表現，但它仍然參與調參。
-
-# 3️⃣ 測試集 (Test set)
-# 完全獨立，最後才用。
-# 不會用來訓練，也不會用來調參。
-# 目的是：模擬「真實世界未來病人」的狀況，評估模型的 最終泛化能力。
-# 只在你模型確定好（hyperparameters fixed）後，才會對測試集做評估並報告 AUC / AUPRC / F1。
-
-# 🔎 舉例（MIMIC-IV CKD ICU 低鈉預測）
-
-# 有 10,000 個 ICU stay。
-# 可能切法
-# 訓練集 (Train)：70% → 用來學習權重。
-# 驗證集 (Validation)：15% → 用來調參、early stopping。
-# 測試集 (Test)：15% → 只用一次，最後報告結果。
